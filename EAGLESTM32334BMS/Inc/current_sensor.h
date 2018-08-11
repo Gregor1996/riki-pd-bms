c2e1a37b58554cb15fa0d14ea074c71f6acfacec
rykymai <rykymai@gmail.com>
2018-08-11 18:53:02 +0200

    current sensor
    This reverts commit 16ca6a318d75f90673dc45cbf35f1e16d9125560.

--- EAGLESTM32334BMS/Inc/current_sensor.h
+++ EAGLESTM32334BMS/Inc/current_sensor.h
@@ -7,7 +7,15 @@
 
 #ifndef CURRENT_SENSOR_H_
 #define CURRENT_SENSOR_H_
+#include "stm32f3xx_hal.h"
+#include "stm32f3xx_hal_adc.h"
+
+#define SENSOR_OFFSET 2068 // ADC at 0A - see static int MEASUREMENT_ScaleAmps(int rawval)
 
 
 
+uint32_t Get_Amps_Value(uint32_t *Vout,uint16_t offset);
+static int MEASUREMENT_ScaleAmps1(int rawval,uint16_t sensor_offset);
+static int MEASUREMENT_ScaleAmps(int rawval);
+float Current_Calibration_Offset(ADC_HandleTypeDef hadc1);
 #endif /* CURRENT_SENSOR_H_ */
--- EAGLESTM32334BMS/Src/current_sensor.c
+++ EAGLESTM32334BMS/Src/current_sensor.c
@@ -5,4 +5,5 @@
  *      Author: Utente
  */
 
+#include "current_sensor.h"
 
